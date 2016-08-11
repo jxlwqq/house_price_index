c1177bb295bfd628f0f9f48a4f8a301209b941f9
xiaolong.jin <jinxl@pingansec.com>
2016-08-11 17:37:09 +0800

    添加一些数据

--- run.py
+++ run.py
@@ -9,4 +9,4 @@ if __name__ == '__main__':
     # 百度指数
     main.main('上海', '')
     # 大中城市房屋价格指数
-    house_price_index.main('北京', '2016-05')
+    #house_price_index.main('北京', '2016-05')
--- webapp/web.py
+++ webapp/web.py
@@ -1,9 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 from flask import Flask, render_template
+import model
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', name=123)
+    sql = "select time_index, data_value from house.house_index where data_key = '百度指数-二手房' and area_id = 18 order by time_index asc"
+    res = model.query(sql)
+    date = []
+    data = []
+    for val in res:
+        date.append(int(val[0]))
+        data.append(int(val[1]))
+
+    return render_template('index.html', date=date, data=data, title=u'百度指数-二手房')
 
 
 if __name__ == '__main__':
